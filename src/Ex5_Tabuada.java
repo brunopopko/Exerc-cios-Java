e9fe15e4ec9c2e1ffc86f7b5ca9cb17f8615bbd7
Perkles <bruno.popko@gmail.com>
2023-01-02 18:18:41 -0300

    Teste

--- src/Ex5_Tabuada.java
+++ src/Ex5_Tabuada.java
@@ -19,9 +19,8 @@ public class Ex5_Tabuada {
 
         System.out.println("Tabuada: ");
         int tabuada = scan.nextInt();
-        
         System.out.println("Tabuada de: " + tabuada);
-
+        
         for(int i = 1; i <= 10; i++) {
             System.out.println(tabuada + "x" + i + "=" + (tabuada*i));
         }
